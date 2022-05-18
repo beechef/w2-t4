dacb2628e8f1e40a9cfa85baa3ada25f505cb4c9
Chef <andelnguyen@gmail.com>
2022-05-18 21:08:19 +0700

    Finish add_more_clocks

--- Assets/Scripts/ClockCreator.cs
+++ Assets/Scripts/ClockCreator.cs
@@ -8,14 +8,14 @@ public class ClockCreator : MonoBehaviour
     public float SPACE;
     void Start()
     {
-        //GameObject clock;
-        //for (int i = 0; i < 12; i++)
-        //{
-        //    clock = Instantiate(ClockPrefab, transform);
-        //    clock.name = "Time Zone: " + i.ToString();
-        //    clock.transform.Translate(Vector3.left * i * SPACE, Space.World);
-        //    clock.GetComponent<ClockController>().TimeZone = i;
-        //}
+        GameObject clock;
+        for (int i = 0; i < 12; i++)
+        {
+            clock = Instantiate(ClockPrefab, transform);
+            clock.name = "Time Zone: " + i.ToString();
+            clock.transform.Translate(Vector3.left * i * SPACE, Space.World);
+            clock.GetComponent<ClockController>().TimeZone = i;
+        }
     }
 
     void Update()
