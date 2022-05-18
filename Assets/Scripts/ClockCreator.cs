696ba12de6edb89eb3a440c9abb9ef45f272b585
Chef <andelnguyen@gmail.com>
2022-05-18 21:07:11 +0700

    Finish create_clock

---
+++ Assets/Scripts/ClockCreator.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockCreator : MonoBehaviour
+{
+    public GameObject ClockPrefab;
+    public float SPACE;
+    void Start()
+    {
+        //GameObject clock;
+        //for (int i = 0; i < 12; i++)
+        //{
+        //    clock = Instantiate(ClockPrefab, transform);
+        //    clock.name = "Time Zone: " + i.ToString();
+        //    clock.transform.Translate(Vector3.left * i * SPACE, Space.World);
+        //    clock.GetComponent<ClockController>().TimeZone = i;
+        //}
+    }
+
+    void Update()
+    {
+
+    }
+}
